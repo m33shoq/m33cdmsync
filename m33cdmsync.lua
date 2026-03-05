5b8d586484dc2a568d38314b4c1f651a35691c58
m33shoq <mishadyrov@gmail.com>
2026-03-06 01:06:16 +0200

    big update, no realoads, addon will have to re-remeber layouts on first login

--- m33cdmsync.lua
+++ m33cdmsync.lua
@@ -36,71 +36,15 @@ local function EncodeTable(data)
 	return true, encoded;
 end
 
-local getLayoutByName = function(layoutName)
-	local CVS = CooldownViewerSettings
-	local LM = CVS:GetLayoutManager()
-	for _, layout in pairs(LM.layouts) do
-		---@cast layout CDMLayout
-		if layout.layoutName == layoutName then
-			return layout
-		end
-	end
-end
-
-local removeLayout = function(layoutID)
-	local CVS = CooldownViewerSettings
-	local LM = CVS:GetLayoutManager()
-
-	LM:RemoveLayout(layoutID)
-	CVS:SaveCurrentLayout();
-end
-
-local getCurrentSpecTag = function()
-	local CVS = CooldownViewerSettings
-	local LM = CVS:GetLayoutManager()
-
-	return LM.currentSpecTag
-end
-
 local function saveLayouts()
-	local CVS = CooldownViewerSettings
-	local LM = CVS:GetLayoutManager()
-
-	local expTable = {}
-
-	for _, layout in pairs(LM.layouts) do
-		local layoutID = layout.layoutID
-		local layoutName = layout.layoutName
-		local ok, rawencoded = EncodeTable(layout)
-
-		local importStr = LM:GetSerializer():SerializeLayouts(layoutID)
-		tinsert(expTable, {
-			importStr = importStr,
-			layoutName = layoutName,
-			rawencoded = rawencoded,
-		})
-	end
+	local expTable = {
+        rawencoded = C_CooldownViewer.GetLayoutData()
+    }
 
 	local ok, exp = EncodeTable(expTable)
 	assert(ok, exp)
 	M33CDMSYNCSTRINGS[UnitClassBase("player")] = exp
 end
-
-local function deepCompare(t1, t2)
-	if type(t1) ~= type(t2) then return false end
-	if type(t1) ~= "table" then return t1 == t2 end
-
-	local keys = {}
-	for k in pairs(t1) do keys[k] = true end
-	for k in pairs(t2) do keys[k] = true end
-
-	for k in pairs(keys) do
-		if not deepCompare(t1[k], t2[k]) then return false end
-	end
-
-	return true
-end
-
 
 local function restoreLayouts()
 	local imp = M33CDMSYNCSTRINGS[UnitClassBase("player")]
@@ -108,84 +52,31 @@ local function restoreLayouts()
 	local ok, impTable = DecodeTable(imp)
 	assert(ok, impTable)
 
-	local shouldReload = false
+    if not impTable.rawencoded then
+        print("[M33CDMSync] No layout data found in the decoded table, aborting import")
+        return
+    end
 
-	for _, layoutData in ipairs(impTable) do
-		local CVS = CooldownViewerSettings
-		local LM = CVS:GetLayoutManager()
-		local layoutName = layoutData.layoutName
-		local importStr = layoutData.importStr
+    if type(impTable.rawencoded) ~= "string" then
+        print("[M33CDMSync] Invalid layout data format, expected a string, aborting import")
+        return
+    end
 
-		local oldLayout = getLayoutByName(layoutName)
-		local shouldImport = true
-		while oldLayout do
-			local ok, decodedLayout = DecodeTable(layoutData.rawencoded)
-			local eq = deepCompare(oldLayout, decodedLayout)
-			if eq then
-				print("[M33CDMSync] Layout", layoutName, "is identical, skipping import")
-				shouldImport = false
-				break
-			end
-			removeLayout(oldLayout.layoutID) -- need to remove old profile with same name first for updating to work and not be confusing
-            print("[M33CDMSync] Removed old layout", layoutName, "width id", oldLayout.layoutID)
-			oldLayout = getLayoutByName(layoutName)
-		end
+    if impTable.rawencoded == C_CooldownViewer.GetLayoutData() then
+        print("[M33CDMSync] Layout data is identical to current, skipping import")
+        return
+    end
 
-		if shouldImport then
-			print("[M33CDMSync] Importing layout:", layoutName)
-
-			local layoutIDs = LM:CreateLayoutsFromSerializedData(importStr)
-			local importedLayout = LM:GetLayout(layoutIDs[1])
-
-			local currentSpecTag = getCurrentSpecTag()
-			if currentSpecTag == importedLayout.classAndSpecTag then
-				CVS:SetActiveLayoutByID(importedLayout.layoutID)
-			end
-			LM:SaveLayouts()
-			shouldReload = true
-		end
-	end
-
-	if shouldReload then
-		StaticPopupDialogs["M33_CDM_SYNC_RELOAD"] = {
-			text = "M33CDMSync: Layouts have been updated. Please reload your UI to avoid taint.",
-			button1 = "Reload Now",
-			button2 = CANCEL,
-			OnAccept = ReloadUI,
-			timeout = 0,
-			whileDead = true,
-			hideOnEscape = false,
-			showAlert = true,
-		}
-		StaticPopup_Show("M33_CDM_SYNC_RELOAD")
-	end
+    C_CooldownViewer.SetLayoutData(impTable.rawencoded)
+    print("[M33CDMSync] Layout data reimported")
 end
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_LOGOUT")
-f:RegisterEvent("VARIABLES_LOADED")
-f:RegisterEvent("PLAYER_ENTERING_WORLD")
-f:RegisterEvent("COOLDOWN_VIEWER_DATA_LOADED")
-local vl, pew, cvdl
 f:SetScript("OnEvent", function(self, event, ...)
 	if event == "PLAYER_LOGOUT" then
 		saveLayouts()
-		return
-	elseif event == "VARIABLES_LOADED" then
-		vl = true
-        self:UnregisterEvent("VARIABLES_LOADED")
-	elseif event == "PLAYER_ENTERING_WORLD" then
-		local isInitialLogin, isReloadingUi = ...
-		if not isInitialLogin and not isReloadingUi then return end
-		pew = true
-        self:UnregisterEvent("PLAYER_ENTERING_WORLD")
-	elseif event == "COOLDOWN_VIEWER_DATA_LOADED" then
-		cvdl = true
-        self:UnregisterEvent("COOLDOWN_VIEWER_DATA_LOADED")
-	end
-	if vl and pew and cvdl then
-		C_Timer.After(6, function()
-			restoreLayouts()
-		end)
-	end
+    end
 end)
+
+restoreLayouts()
